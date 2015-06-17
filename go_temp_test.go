16c5f8b0b0d04d70c3ef0464a0a339b58cecf50f
Nick Robinson <grobins2@gmail.com>
2015-06-17 14:03:22 -0400

    wip

--- go_temp_test.go
+++ go_temp_test.go
@@ -1,12 +1,18 @@
 package temp_test
 
 import (
-	. "temp"
+	. "github.com/cryptojuice/go-temp"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Temp", func() {
-
+	Describe("Create", func() {
+		It("", func() {
+			f, err := Create()
+			Expect(err).To(BeNil())
+			Expect(f.Name()).To(Equal("temp123"))
+		})
+	})
 })
--- temp.go
+++ temp.go
@@ -1,10 +1,31 @@
 package temp
 
-import "os"
+import (
+	"math/rand"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+)
 
-type File struct {
+func Create() (file *os.File, err error) {
+	s := []string{"tmp-", generateRandomString()}
+	f, err := os.Create(strings.Join(s, ""))
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
+	go func() {
+		<-c
+		err := os.Remove(f.Name())
+		if err != nil {
+			os.Exit(0)
+		}
+		os.Exit(1)
+	}()
+	return f, err
 }
 
-func Create() (f *os.File, err error) {
-	return os.Create("temp123")
+func generateRandomString() (s string) {
+	return strconv.FormatInt(rand.Int63(), 10)
 }
